758373f791e2cbb4ef361d653834361cba9d2ae1
Jasmine Maria Kiss <jasminemk@uia.no>
2025-04-08 21:18:20 +0200

    step 2 completed

---
+++ models/game.mjs
@@ -0,0 +1,15 @@
+export default class Game {
+    constructor ({ title, designer, artist, publisher, year, players, time, difficulty, url, playCount, personalRating }) {
+        this.title = title;
+    this.designer = designer;
+    this.artist = artist;
+    this.publisher = publisher;
+    this.year = year;
+    this.players = players;
+    this.time = time;
+    this.difficulty = difficulty;
+    this.url = url;
+    this.playCount = playCount;
+    this.personalRating = personalRating;
+  }
+}
