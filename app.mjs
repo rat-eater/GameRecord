cf2057f5eec965472eb19426c512c26d9c8d7879
Jasmine Maria Kiss <jasminemk@uia.no>
2025-04-08 21:22:25 +0200

    updated app.mjs based on the example given

---
+++ app.mjs
@@ -0,0 +1,17 @@
+import Game from './models/game.mjs';
+
+const exampleGame = new Game({
+    title: "Ticket to Ride",
+    designer: "Alan R. Moon",
+    artist: "Julien Delval, Cyrille Daujean",
+    publisher: "Days of Wonder",
+    year: 2004,
+    players: "2–5",
+    time: "30–60 mins",
+    difficulty: "Light",
+    url: "https://boardgamegeek.com/boardgame/9209/ticket-ride",
+    playCount: 423,
+    personalRating: 6
+  });
+
+  console.log(exampleGame);
