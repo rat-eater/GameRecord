2e136330f6b07c3cc1f46735f5ea13413fbddf9d
Jasmine Maria Kiss <jasminemk@uia.no>
2025-04-08 22:13:23 +0200

    created a test function

--- app.mjs
+++ app.mjs
@@ -1,21 +1,5 @@
 import Game from "./models/game.mjs";
 
-const testGame = new Game({
-    title: "Test Game",
-    designer: "Tester",
-    artist: "Art Tester",
-    publisher: "Test Inc.",
-    year: 2025,
-    players: "2-4",
-    time: "45 mins",
-    difficulty: "Easy",
-    url: "http://example.com/testgame",
-    playCount: 1,
-    personalRating: 5
-  });
-  
-  console.log("Test game:", testGame);  
-
   function saveGame(game) {
     const key = "game_" + game.title.replaceAll(" ", "");
     localStorage.setItem(key, JSON.stringify(game));
@@ -51,3 +35,23 @@ const testGame = new Game({
         saveGame(game);
     });
   }
+
+  // Test
+
+const testGame = new Game({
+    title: "Test Game",
+    designer: "Tester",
+    artist: "Art Tester",
+    publisher: "Test Inc.",
+    year: 2025,
+    players: "2–4",
+    time: "45 mins",
+    difficulty: "Easy",
+    url: "http://example.com/testgame",
+    playCount: 1,
+    personalRating: 5
+  });
+  
+  saveGame(testGame); 
+  console.log("Games in localStorage:", getAllGames()); 
+  
